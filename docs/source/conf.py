70c2219aecd14737bc88bbb237fac6c2e9997b3e
LUHMW <132049891+LUHMW@users.noreply.github.com>
2023-09-29 12:39:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ html_logo = 'logo_nfdi4ing_cmyk_hoch.svg'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
-    'table_style': 'width: 50%;',
+    'table_style': 'width: 80%;',
     }
 
 # -- Options for EPUB output
