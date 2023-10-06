372638b8bc970f77900e2eef512d74fbc16f095b
LUHMW <132049891+LUHMW@users.noreply.github.com>
2023-10-06 15:54:32 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,6 @@ html_logo = 'logo_nfdi4ing_cmyk_hoch.svg'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
-    'body_max_width': '50%',
     }
 
 # -- Options for EPUB output
